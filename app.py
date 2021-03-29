9e68c869ac8b13034f3723b0426041a0558fbf1e
Justin Ho <github@jcsh.dev>
2021-03-29 00:32:21 -0700

    refactor: remove hello world method

--- app.py
+++ app.py
@@ -11,10 +11,6 @@ from api import *
 app.register_blueprint(bus_controller)
 app.register_blueprint(stop_controller)
 
-@app.route('/')
-def hello():
-    return "Hello World!"
-
 print(os.environ['APP_SETTINGS'])
     
 if __name__ == '__main__':
